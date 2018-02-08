cd4107342d84d921e22d53db5ae444cc5c1af873
marius.degen <marius.degen@accenture.com>
2018-02-08 18:26:43 +0100

    update pipeline scripts - moved timestamps block and added dir statement

--- pipelineBuilds/ComponentsSnapshot.groovy
+++ pipelineBuilds/ComponentsSnapshot.groovy
@@ -1,8 +1,9 @@
 package main
 
-node {
-    timestamps {
-        try {
+node { 
+    try {
+        timestamps {
+            cleanWs()
             currentBuild.result = 'SUCCESS'
             checkout([$class: 'GitSCM', branches: [[name: 'refs/heads/master']], browser: [$class: 'GitWeb', repoUrl: '/usr/share/gitweb'], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'myID', url: 'http://172.31.22.80/gitweb/hpsapf-components.git']]])
 
@@ -10,55 +11,57 @@ node {
             //TODO: Check if a failure in Pre Steps fails the build
             // This step is defined twice in the ui once without any parameters
             echo 'INFO: Setting build name'
-            pom = readMavenPom file: 'pom.xml'
-            currentBuild.displayName = "$pom.version($env.BUILD_NUMBER)"
+            dir ('acn-hpsapf-components-parent') {
+                pom = readMavenPom file: 'pom.xml'
+                currentBuild.displayName = "$pom.version($env.BUILD_NUMBER)"
 
-            //Invoke top-level Maven targets
-            echo 'INFO: Executing Maven deploy'
-            withMaven {
-                //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
-                sh "mvn clean deploy -Dmaven.buildmode=ci -Dfile.encoding=UTF-8 -DaltDeploymentRepository=snapshots::default::http://172.31.22.80:8081/nexus/content/repositories/snapshots/ -B -X"
-            }
-
-            //Build
-            try {
+                //Invoke top-level Maven targets
                 echo 'INFO: Executing Maven deploy'
                 withMaven {
                     //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
                     sh "mvn clean deploy -Dmaven.buildmode=ci -Dfile.encoding=UTF-8 -DaltDeploymentRepository=snapshots::default::http://172.31.22.80:8081/nexus/content/repositories/snapshots/ -B -X"
                 }
-            } catch (e) {
-                //TODO: Check the right test result directory
-                step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
-                step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
-                if (currentBuild.result == 'UNSTABLE') {
-                    echo 'WARNING: Maven build is UNSTABLE'
-                } else {
-                    echo 'WARNING: Maveen build FAILED'
-                    currentBuild.result = 'FAILURE'
-                }
-            }
 
-            //Post Steps
-            //TODO: Check if a Post Step failure immediately fails the build
-            if (currentBuild.result == 'SUCCESS') {
-                //Maven artifacts?
-                echo 'INFO: Executing Maven deploy'
-                withMaven {
-                    //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
-                    sh "mvn clean deploy -Dmaven.buildmode=ci -Dfile.encoding=UTF-8 -DaltDeploymentRepository=snapshots::default::http://172.31.22.80:8081/nexus/content/repositories/snapshots/ -B -X"
+                //Build
+                try {
+                    echo 'INFO: Executing Maven deploy'
+                    withMaven {
+                        //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
+                        sh "mvn clean deploy -Dmaven.buildmode=ci -Dfile.encoding=UTF-8 -DaltDeploymentRepository=snapshots::default::http://172.31.22.80:8081/nexus/content/repositories/snapshots/ -B -X"
+                    }
+                } catch (e) {
+                    //TODO: Check the right test result directory
+                    step([$class: 'ArtifactArchiver', artifacts: '**/target/*.jar', fingerprint: true])
+                    step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+                    if (currentBuild.result == 'UNSTABLE') {
+                        echo 'WARNING: Maven build is UNSTABLE'
+                    } else {
+                        echo 'WARNING: Maveen build FAILED'
+                        currentBuild.result = 'FAILURE'
+                    }
                 }
-                echo 'INFO: Executing Maven sonar check'
-                withMaven {
-                    //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
-                    sh "mvn sonar:sonar -B"
+
+                //Post Steps
+                //TODO: Check if a Post Step failure immediately fails the build
+                if (currentBuild.result == 'SUCCESS') {
+                    //Maven artifacts?
+                    echo 'INFO: Executing Maven deploy'
+                    withMaven {
+                        //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
+                        sh "mvn clean deploy -Dmaven.buildmode=ci -Dfile.encoding=UTF-8 -DaltDeploymentRepository=snapshots::default::http://172.31.22.80:8081/nexus/content/repositories/snapshots/ -B -X"
+                    }
+                    echo 'INFO: Executing Maven sonar check'
+                    withMaven {
+                        //Parent POM location com.accenture.hpsapf.dsl.parent/pom.xml
+                        sh "mvn sonar:sonar -B"
+                    }
                 }
             }
-        } finally {
-            //Check if the Post-build Actions should always be reachable
-            /*if (currentBuild.result == 'SUCCESS' || currentBuild.result == 'UNSTABLE') {
-                def triggeredJob = build job: 'hpsapf referenceapp snapshot build'
-            }*/
         }
+    } finally {
+        //Check if the Post-build Actions should always be reachable
+        /*if (currentBuild.result == 'SUCCESS' || currentBuild.result == 'UNSTABLE') {
+            def triggeredJob = build job: 'hpsapf referenceapp snapshot build'
+        }*/
     }
 }
