881d5d9eadf75aa731f4bb160f491f1bd7dbed1a
marius.degen <marius.degen@accenture.com>
2018-02-08 18:30:59 +0100

    fixed missing braces

--- pipelineBuilds/DSLPluginSnapshot.groovy
+++ pipelineBuilds/DSLPluginSnapshot.groovy
@@ -30,16 +30,16 @@ node {
             //Post Steps
             if (currentBuild.result == 'SUCCESS') {
                 sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\nmkdir /tmp/$temp_dir_name\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.updatesite&v=$POM_VERSION&p=zip\"\nrm -rf /srv/www/updatesite/development/*\nunzip -o /tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip -d /srv/www/updatesite/development\nrm -rf /tmp/$temp_dir_name'
-                dir ('com.accenture.hpsapf.dsl.parent') {}
+                dir ('com.accenture.hpsapf.dsl.parent') {
                     pom = readMavenPom file: 'pom.xml'
                     currentBuild.displayName = "$pom.version($env.BUILD_NUMBER)"
                 }
                 sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\nmkdir /tmp/$temp_dir_name\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.helpdsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.helpdsl.updatesite&v=$POM_VERSION&p=zip\"'
             }
-        } finally {
-            /*if (currentBuild.result == 'SUCCESS') {
-                def triggeredJob = build job: 'hpsapf-arch.git snapshot', wait: false
-            }*/
         }
+    } finally {
+        /*if (currentBuild.result == 'SUCCESS') {
+            def triggeredJob = build job: 'hpsapf-arch.git snapshot', wait: false
+        }*/
     }
 }
