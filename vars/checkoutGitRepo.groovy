5303fa5cb58ac93214f50a9557dee65fd1141d43
marius.degen <marius.degen@accenture.com>
2018-02-12 20:11:22 +0100

    added missing ,

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -1,5 +1,5 @@
 //TODO: Check parameter defalts
 def call(repoURL = 'http://172.31.22.80/gitweb/', credentialsID = '', branch = 'refs/heads/master', browser = 'GitWeb', browserURL = '', browserVersion = '') {
 
-	checkout([$class: 'GitSCM', branches: [[name: branch]], browser: [$class: browser, repoUrl: browserURL version: browserVersion], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: repoURL]]])
+	checkout([$class: 'GitSCM', branches: [[name: branch]], browser: [$class: browser, repoUrl: browserURL, version: browserVersion], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: credentialsId, url: repoURL]]])
 }
