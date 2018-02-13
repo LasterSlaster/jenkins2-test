7bf8ef7ba8da54530f4ea61887098a659ef30142
marius.degen <marius.degen@accenture.com>
2018-02-13 11:35:53 +0100

    added carriage retrun to shell script string

--- pipelineBuilds/ConfiguredDSLPluginSnapshot.groovy
+++ pipelineBuilds/ConfiguredDSLPluginSnapshot.groovy
@@ -14,12 +14,12 @@ configMavenBuild {
 	postStepsScript = {
 		if (currentBuild.result == 'SUCCESS') {
 			echo 'INFO: Executing shell'
-	        sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\nmkdir /tmp/$temp_dir_name\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.updatesite&v=' + env.POM_VERSION + '&p=zip\"\nrm -rf /srv/www/updatesite/development/*\nunzip -o /tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip -d /srv/www/updatesite/development\nrm -rf /tmp/$temp_dir_name'
+	        sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\r\nmkdir /tmp/$temp_dir_name\r\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.updatesite&v=' + env.POM_VERSION + '&p=zip\"\r\nrm -rf /srv/www/updatesite/development/*\r\nunzip -o /tmp/$temp_dir_name/com.accenture.hpsapf.dsl.updatesite.zip -d /srv/www/updatesite/development\r\nrm -rf /tmp/$temp_dir_name'
 		
 			updateBuildName('com.accenture.hpsapf.dsl.parent/')
 
 			echo 'INFO: Executing shell'
-            sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\nmkdir /tmp/$temp_dir_name\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.helpdsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.helpdsl.updatesite&v=' + env.POM_VERSION + '&p=zip\"'
+            sh 'temp_dir_name=temp_zs_`date +%d-%m-%y_%H-%M-%S`\r\nmkdir /tmp/$temp_dir_name\r\nwget -S -O \"/tmp/$temp_dir_name/com.accenture.hpsapf.dsl.helpdsl.updatesite.zip\" \"http://172.31.22.80:8081/nexus/service/local/artifact/maven/redirect?r=snapshots&g=com.accenture.hpsapf&a=com.accenture.hpsapf.dsl.helpdsl.updatesite&v=' + env.POM_VERSION + '&p=zip\"'
 		}
 	} // steps to execute after the maven build
 	postBuildActionsScript = {} // steps to execute after the build process
