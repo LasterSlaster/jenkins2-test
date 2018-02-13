2b565306115b3400a726abd181b69d4a2274657c
marius.degen <marius.degen@accenture.com>
2018-02-13 18:28:07 +0100

    blub

--- vars/triggerParameterizedJob.groovy
+++ vars/triggerParameterizedJob.groovy
@@ -5,13 +5,18 @@ def call(def jobName, def parameters, def wait = true) {
 	echo 'INFO: Triggering job ' + jobName	
    	jobStatus = build job: jobName, parameters: parametersList, wait: wait, propagate: false
     if (wait) {
-        if (jobStatus.result == 'FAILURE') {
-            currentBuild.result = 'FAILURE'
-            //Similar to throwing an error but no stack trace is printed
-            error('ERROR: Triggered Job ' + jobName + ' FAILED')
-        } else if (jobStatus.result == 'UNSTABLE') {
-            currentBuild.result = 'UNSTABLE'
-            echo 'WARNING: Triggered Job ' + jobName + ' is UNSTABLE'
+        switch (jobStatus.result) {
+            case 'FAILURE':
+                currentBuild.result = 'FAILURE'
+                error('ERROR: Triggered Job ' + jobName + ' FAILED')
+                break
+            case 'UNSTABLE':
+                currentBuild.result = 'UNSTABLE'
+                echo 'WARNING: Triggered Job ' + jobName + ' is UNSTABLE'
+                break
+            case 'SUCCESS':
+                currentBuild.result = 'SUCCESS'
+                echo 'INFO: Triggered Job ' + jobName + ' was SUCCESSFULL'
         }
     }
 }
